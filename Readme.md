90a03184b94700045f25e0489b68a49eaf7a0326
kenny-kerson <kenny.kerson@gmail.com>
2023-10-18 00:13:17 +0900

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# 트랜잭션 커밋 후 동작하는 Handler 구현하기 
+
+## Problem
+
+## Goal
+
+## Requirements
+
+## Context
+
+## Solution
+
+## Material Reference
---
+++ src/main/java/com/kenny/transactionaleventlistenerkafkapoc/TransactionaleventlistenerKafkaPocApplication.java
@@ -0,0 +1,13 @@
+package com.kenny.transactionaleventlistenerkafkapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionaleventlistenerKafkaPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionaleventlistenerKafkaPocApplication.class, args);
+    }
+
+}
